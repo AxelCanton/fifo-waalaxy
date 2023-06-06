2f1eb0c9596be4410c67c5f55f9fd3002e6bb6b0
AxelCanton <axel.canton@koovea.com>
2023-06-06 11:33:59 +0200

    rm logs

--- server/helpers.ts
+++ server/helpers.ts
@@ -4,12 +4,10 @@ import { removeFromQueue, setNewCredits } from './data';
 export const setCronJobs = () => {
     // runs every day at 00:00
     cron.schedule('0 0 1-31 * *', () => {
-        console.log('new credits')
         setNewCredits();
     });
 
     cron.schedule('*/2 * * * *', () => {
-        const a = removeFromQueue();
-        console.log('removed: ', a);
+        removeFromQueue();
     });
 };
